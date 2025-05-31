515c22b4940be1827204eaa2cf8cbc2fcda43771
wz <3565087505@qq.com>
2025-05-31 10:29:28 +0800

    finish

--- src/test/java/com/wzres/SpringTest.java
+++ src/test/java/com/wzres/SpringTest.java
@@ -1,6 +1,7 @@
 package com.wzres;
 
 import com.wzres.config.UserConfig;
+import com.wzres.domian.User;
 import com.wzres.service.UserService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -27,5 +28,12 @@ public class SpringTest {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
         UserService userServiceImpl = applicationContext.getBean("userServiceImpl", UserService.class);
         userServiceImpl.editUser(1,1);
+    }
+
+    @Test
+    public void testStart(){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        User userBean = applicationContext.getBean("userBean", User.class);
+        System.out.println(userBean);
     }
 }
